0b321ff0bf5762f3fc502822fd7a9a86cb0146cb
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-10-24 01:15:21 +0900

    chore: MDC 로깅 및 Logback 설정 추가

---
+++ monew-api/src/main/java/com/monew/monew_api/common/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.monew.monew_api.common.config;
+
+import com.monew.monew_api.common.interceptor.MDCLoggingInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final MDCLoggingInterceptor mdcLoggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(mdcLoggingInterceptor)
+                .addPathPatterns("/**");
+    }
+}
