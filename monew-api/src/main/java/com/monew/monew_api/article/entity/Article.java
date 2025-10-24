7f09bffef55ec52c7111d2edac3b4617201ee2a8
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-10-24 15:47:43 +0900

    feat: Article, ArticleView 엔티티 추가

---
+++ monew-api/src/main/java/com/monew/monew_api/article/entity/Article.java
@@ -0,0 +1,46 @@
+package com.monew.monew_api.article.entity;
+
+import com.monew.monew_api.common.entity.BaseIdEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * 뉴스 기사 테이블
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "articles")
+public class Article extends BaseIdEntity {
+
+    @Column(nullable = false, length = 20)
+    private String source;
+
+    @Column(name = "source_url", nullable = false, length = 500, unique = true)
+    private String sourceUrl;
+
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @Column(name = "publish_date", nullable = false)
+    private LocalDateTime publishDate;
+
+    @Column(nullable = false, length = 200)
+    private String summary;
+
+    @Column(name = "comment_count", nullable = false)
+    private int commentCount = 0;
+
+    @Column(name = "view_count", nullable = false)
+    private int viewCount = 0;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
+}
