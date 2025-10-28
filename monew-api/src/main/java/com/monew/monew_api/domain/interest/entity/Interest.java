0f9aba35127b2ee476c4ce8dd44b53577ce7a205
yelim-Lee <dpfla1078@naver.com>
2025-10-28 15:51:40 +0900

    refactor : swaggerUI에 맞춰 변수 변경

--- monew-api/src/main/java/com/monew/monew_api/domain/interest/entity/Interest.java
+++ monew-api/src/main/java/com/monew/monew_api/domain/interest/entity/Interest.java
@@ -5,8 +5,11 @@ import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,6 +32,7 @@ public class Interest extends BaseTimeEntity {
   private int subscriberCount = 0;
 
   @OneToMany(mappedBy = "interest", cascade = CascadeType.ALL, orphanRemoval = true)
+  @OrderBy("createdAt ASC")
   private Set<InterestKeyword> keywords = new HashSet<>();
 
   private Interest(String name, int subscriberCount){
