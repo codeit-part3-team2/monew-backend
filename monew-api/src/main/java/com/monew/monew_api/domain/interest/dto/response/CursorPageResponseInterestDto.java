8fd053c17cd9235dedee226bc8911d033521faad
yelim-Lee <dpfla1078@naver.com>
2025-10-28 13:07:17 +0900

    refactor : 파라미터 삭제

--- monew-api/src/main/java/com/monew/monew_api/domain/interest/dto/response/CursorPageResponseInterestDto.java
+++ monew-api/src/main/java/com/monew/monew_api/domain/interest/dto/response/CursorPageResponseInterestDto.java
@@ -4,8 +4,8 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record CursorPageResponseInterestDto<T>(
-    List<T> content, // 실제 데이터 목록
+public record CursorPageResponseInterestDto(
+    List<InterestDto> content, // 실제 데이터 목록
     String nextCursor, // 다음 페이지 요청 위한 커서
     LocalDateTime nextAfter, // 커서 기준 시점
     int size, // 한페이지에 담긴 데이터 개수
