c46200c18f303a66ca8983a002a4d7159f7cab91
yelim-Lee <dpfla1078@naver.com>
2025-10-29 11:34:11 +0900

    refactor : 빌더 어노테이션 삭제

--- monew-api/src/main/java/com/monew/monew_api/interest/dto/response/InterestDto.java
+++ monew-api/src/main/java/com/monew/monew_api/interest/dto/response/InterestDto.java
@@ -3,13 +3,12 @@ package com.monew.monew_api.interest.dto.response;
 import java.util.List;
 import lombok.Builder;
 
-@Builder
-public record InterestDto (
+public record InterestDto(
     Long id,
     String name,
     List<String> keywords,
     Long subscriberCount,
     boolean subscribedByMe
-){
+) {
 
 }
