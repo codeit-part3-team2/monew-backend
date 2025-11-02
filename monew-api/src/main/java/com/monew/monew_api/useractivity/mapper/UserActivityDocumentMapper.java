5842e9d93f947d4c2235a8bea604a109a5f92006
userjin2123 <userjin2123@gmail.com>
2025-11-03 00:14:28 +0900

    feat : add 이벤트 리스너, mongoDB cache, reverse-index Document, cache와 revers-index 서비스와 repository 추가

--- monew-api/src/main/java/com/monew/monew_api/useractivity/mapper/UserActivityDocumentMapper.java
+++ monew-api/src/main/java/com/monew/monew_api/useractivity/mapper/UserActivityDocumentMapper.java
@@ -7,6 +7,6 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserActivityDocumentMapper {
-    @Mapping(target = "cachedAt", expression = "java(java.time.LocalDateTime.now())")
+    @Mapping(target = "updatedAt", expression = "java(java.time.LocalDateTime.now())")
     UserActivityCacheDocument toDocument(UserActivityDto dto);
 }
