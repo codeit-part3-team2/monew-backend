dfdb0cdda5261a606be5996e9f70b4c46d5ea61b
JaehyeokLim <217686834+JaehyeokLim@users.noreply.github.com>
2025-11-04 14:39:52 +0900

    chore: 필요없는 빈 Mock으로 처리

--- monew-api/src/test/java/com/monew/monew_api/MonewApiApplicationTests.java
+++ monew-api/src/test/java/com/monew/monew_api/MonewApiApplicationTests.java
@@ -1,13 +1,16 @@
 package com.monew.monew_api;
 
+import com.monew.monew_api.common.config.AWSConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest
+@SpringBootTest(classes = MonewApiApplication.class)
 class MonewApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@MockBean
+	private AWSConfig awsConfig;
 
+	@Test
+	void contextLoads() {}
 }
